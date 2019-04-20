d51a31be588bf72282d9bd3af0acf48b391b93a9
fito <fifodominguez@gmail.com>
2019-04-20 13:05:08 -0600

    Updates

---
+++ precache-manifest.dff1fa3b7d3abdd5bf13128f9c27dfc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa6034d5373d108f4b91",
+    "url": "/cotizador/static/css/main.de2a1fa3.chunk.css"
+  },
+  {
+    "revision": "aa6034d5373d108f4b91",
+    "url": "/cotizador/static/js/main.299d33aa.chunk.js"
+  },
+  {
+    "revision": "9d1dc2de9aa60c4a23b0",
+    "url": "/cotizador/static/js/runtime~main.ac779f68.js"
+  },
+  {
+    "revision": "05895d03497606e54724",
+    "url": "/cotizador/static/js/2.52b8f231.chunk.js"
+  },
+  {
+    "revision": "da82ca1555694ab7640bbd31b9ebf9e3",
+    "url": "/cotizador/index.html"
+  }
+];
